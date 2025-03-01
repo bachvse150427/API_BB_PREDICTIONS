d846c7703d16fdaeda4fb09d7ee06e306c35717b
ALAN <bachvse150427@fpt.edu.vn>
2025-03-01 16:50:50 +0700

    Make logging -> exception

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@
 Setup file for the API_PREDICTIONS package
 '''
 
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements() -> List[str]:
+def get_requirements()->List[str]:
     """
-    This function will return list of requirements
+    Thiss function will return list of requirements
+    
     """
-    requirement_lst: List[str] = []
+    requirement_lst:List[str]=[]
     try:
-        with open('requirements.txt', 'r') as file:
-            # Read lines from the file
-            lines = file.readlines()
-            # Process each line
+        with open('requirements.txt','r') as file:
+            #Read lines from the file
+            lines=file.readlines()
+            ## Process each line
             for line in lines:
-                requirement = line.strip()
-                # ignore empty lines and -e .
-                if requirement and requirement != '-e .':
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
         print("requirements.txt file not found")
